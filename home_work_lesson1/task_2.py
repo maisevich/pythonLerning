4e99ca6643947e12a7b53bd1ac7a54457faf5a3d
Alex Maisevich <maisevich.a.s@gmail.com>
2022-09-25 00:12:41 +0300

    commit_task_2_home_work

--- home_work_lesson1/task_2.py
+++ home_work_lesson1/task_2.py
@@ -1,3 +1,18 @@
 #2. Напишите программу для. проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-
+def num(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+def predicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+stat = num(3)
+if predicate(stat) == True:
+    print("Истинно")
+else:
+    print("Ложно")
